0bd509517c0d5c86f415be7524bc56458ab4b58e
David <slayne31@gmail.com>
2015-11-06 08:47:01 +0100

    add user

--- Server.java
+++ Server.java
@@ -1,5 +1,84 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.ArrayList;
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.ArrayList;
+
 /**
  * Created by david on 06/11/2015.
  */
 public class Server {
+
+    public static void main(String[] args) {
+        Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "ConnectionFactory";
+        String destName = "bourse";
+        Destination dest = null;
+        int count = 5;
+        Session session = null;
+        MessageProducer sender = null;
+
+
+
+
+        try {
+            // create the JNDI initial context.
+            context = new InitialContext();
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                    false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the sender
+            sender = session.createProducer(dest);
+
+            // start the connection, to enable message sends
+            connection.start();
+
+            ArrayList<TitreBoursier> l= TitreBoursier.generateTitres();
+            for (int i = 0; i < l.size(); ++i) {
+                TextMessage message = session.createTextMessage();
+                message.setText(l.get(i).showCours());
+                sender.send(message);
+                System.out.println("Sent: " + message.getText());
+            }
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
 }
